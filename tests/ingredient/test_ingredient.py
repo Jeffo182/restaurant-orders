9b9bcece86516b8f13d4dbdf2089416704310636
Jefferson <jefferson.felix182@gmail.com>
2023-06-16 21:33:35 -0300

    fim do dia

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,15 +3,14 @@ from src.models.ingredient import Ingredient # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-
+    # Criando objetos Ingredient com diferentes ingredientes
     ingredient1 = Ingredient("carne")
     ingredient2 = Ingredient("frango")
-    ingredient3 = Ingredient("carne")
-
-    assert ingredient1.name == 'carne'
+    ingredient3 = Ingredient("bacon")
+    # Verificando as propriedades dos objetos Ingredient
+    assert ingredient1.name == "carne"
+    assert ingredient2.name == "frango"
+    assert ingredient3.name == "bacon"
+    ingredient1_copy = Ingredient("queijo mussarela")
+    assert ingredient1 == ingredient1_copy
     assert ingredient1 != ingredient2
-    assert ingredient1 == ingredient3
-    assert hash(ingredient1) != hash(ingredient2)
-    assert hash(ingredient1) == hash(ingredient3)
-    assert str(ingredient1) == "Ingredient('carne')"
-    assert ingredient1.restrictions == set()
